e58d60df42805b6a2f9038d907e7b07f74666f68
Martin Vansa <martin.vansa@haemonetics.com>
2019-12-10 11:00:20 +0100

    first commit to gitHub

--- src/main/java/com/techprimers/springbootwebsocketexample/resource/RestControler.java
+++ src/main/java/com/techprimers/springbootwebsocketexample/resource/RestControler.java
@@ -1,63 +1,59 @@
 package com.techprimers.springbootwebsocketexample.resource;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.techprimers.springbootwebsocketexample.entity.Sensor_h11;
 import com.techprimers.springbootwebsocketexample.model.UserResponse;
 import com.techprimers.springbootwebsocketexample.repository.Testrepositoryh11;
 import com.techprimers.springbootwebsocketexample.service.ServiceH11;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
 
 @RestController
 @RequestMapping("rest")
 public class RestControler {
 
-    @Autowired
-    private Testrepositoryh11 testrepositoryh11;
+	@Autowired
+	private Testrepositoryh11 testrepositoryh11;
 
-    @Autowired
-    private ServiceH11 serviceH11;
+	@Autowired
+	private ServiceH11 serviceH11;
 
-    @Autowired
-    SimpMessagingTemplate template;
+	@Autowired
+	SimpMessagingTemplate template;
 
-    @RequestMapping(value = "/addSensorH11", params = { "name_temp", "valuee_temp", "name_hum",
-            "valuee_hum" }, method = RequestMethod.POST)
-    public String gameHandler3(@RequestParam("name_temp") String name_temp,
-                               @RequestParam("valuee_temp") double valuee_temp,
-                               @RequestParam("name_hum") String name_hum,
-                               @RequestParam("valuee_hum") double valuee_hum) {
+	@RequestMapping(value = "/addSensorH11", params = { "name_temp", "valuee_temp", "name_hum",
+			"valuee_hum" }, method = RequestMethod.POST)
+	public String gameHandler3(@RequestParam("name_temp") String name_temp,
+			@RequestParam("valuee_temp") double valuee_temp, @RequestParam("name_hum") String name_hum,
+			@RequestParam("valuee_hum") double valuee_hum) {
 
-        Sensor_h11 newdata = null;
-        String createBy = "From  module ESP 32";
-        int i;
-        try {
+		Sensor_h11 newdata = null;
+		String createBy = "From  module ESP 32";
+		int i;
+		try {
+			float sal_temp = (float) valuee_temp;
+			float sal_hum = (float) valuee_hum;
 
-            float sal_temp = (float) valuee_temp;
-            float sal_hum = (float) valuee_hum;
+			if (name_temp != null && name_hum != null) {
+				newdata = serviceH11.addSensorh11(name_temp, sal_temp, name_hum, sal_hum, createBy);
+				testrepositoryh11.save(newdata);
+				template.convertAndSend("/topic/user",
+						new UserResponse(newdata.getValuee_hum(), new Date(), newdata.getValuee_temp()));
+				return null;
+			}
 
-            if (name_temp != null && name_hum != null) {
-                i = 10;
-                newdata = serviceH11.addSensorh11(name_temp, sal_temp, name_hum, sal_hum, createBy);
-                testrepositoryh11.save(newdata);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "There is no such item";
+		}
 
-                template.convertAndSend("/topic/user", new UserResponse(newdata.getValuee_hum(),new Date(), newdata.getValuee_temp() ));
-
-                return null;
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "There is no such item";
-        }
-
-        return "Param {game} is ----" + null;
-    }
-
-
+		return "Param {game} is ----" + null;
+	}
 
 }
